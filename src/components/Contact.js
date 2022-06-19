3ab22642c36e99632123c4efe30addf68114ed21
LimBack <rahmadfirdaus78@gmail.com>
2022-06-19 21:24:23 +0700

    feat: Chalenge ke-9 Rahmad Firdaus

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,15 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = ({data}) => {
     return (
-        <></>
-    )
+        <div myContact="card">
+            <img myContact="photo" src={data.photo} alt="foto"></img>
+            <h3>{data.name}</h3>
+            <p>{data.phone}</p>
+            <p>{data.email}</p>
+        </div>
+    );
 }
 
 export default Contact;
